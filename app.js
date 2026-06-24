07ed335941aae5de1b022260cf1184f4abcaf8a1
Md. Shariful Islam <mohammadsharifulislam64@gmail.com>
2026-06-25 00:06:48 +0600

    feat : added delivery workflow docker registry

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    // res.send('Hello, World!');
-    res.status(201).send('Hello, World!');
+    res.send('Hello, World!');
+    // res.status(201).send('Hello, World!');
 });
 
 module.exports = app;
