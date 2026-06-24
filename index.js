9d6e1e29ed8bccc2e321cb8e7112732cf597a219
Md. Shariful Islam <mohammadsharifulislam64@gmail.com>
2026-06-24 23:22:08 +0600

    feat : added test workflow

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-const express = require('express');
-
-const app = express();
-
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-});
+const http = require('http');
+const app = require('./app'); // Import the Express app
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-});
+})
---
+++ test/app.test.js
@@ -0,0 +1,20 @@
+const request = require('supertest');
+const app = require('../app'); // Adjust the path to your app.js file
+
+describe('GET /', function () {
+    it('responds with text Hello, World!"', async function () {
+        const chai = await import('chai');
+        const expect = chai.expect;
+
+        await request(app)
+            .get('/')
+            .expect(200)
+            .then((response) => {
+                expect(response.text).to.equal('Hello, World!');
+            });
+
+
+
+    })
+
+})
